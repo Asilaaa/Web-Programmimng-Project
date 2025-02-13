647082e5225f6bf81ca004dc1fc323c2f78c409d
Asilaaa <147584814+Asilaaa@users.noreply.github.com>
2025-02-13 12:04:00 +0300

    Add files via upload

---
+++ Books.js
@@ -0,0 +1,25 @@
+const books = [
+    {
+      title: "The Great Gatsby",
+      author: "F. Scott Fitzgerald",
+      description: "A classic novel about the American Dream.",
+      cover: "https://i.pinimg.com/736x/d9/e1/65/d9e1658b173b0b7a8369830acee01cbc.jpg",
+      file: "books/book1.pdf"
+    },
+    {
+      title: "1984",
+      author: "George Orwell",
+      description: "A dystopian novel about totalitarianism.",
+      cover: "https://i.pinimg.com/736x/17/fb/e1/17fbe1b3096863d5af032ce42c80c79c.jpg",
+      file: "books/book2.pdf"
+    },
+    {
+      title: "To Kill a Mockingbird",
+      author: "Harper Lee",
+      description: "A story of racial injustice in the American South.",
+      cover: "https://i.pinimg.com/736x/22/6a/82/226a8218d7ce8c30743a0585ff4b8155.jpg",
+      file: "books/book3.pdf"
+    }
+  ];
+
+  export { books };
